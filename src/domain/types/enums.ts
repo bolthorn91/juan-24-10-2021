87aa998b73a1ac474626bfc10d9e9e710b57842d
bolthorn91 <bolthorn91@gmail.com>
2021-10-19 18:39:31 +0100

    Add socket message enums

---
+++ src/domain/types/enums.ts
@@ -0,0 +1,14 @@
+export enum MESSAGE_EVENTS {
+    SUBSCRIBE = 'subscribe',
+    UNSUBSCRIBE = 'unsubscribe'
+}
+
+export enum MESSAGE_FEEDS {
+    BOOK_SNAPSHOT = 'book_ui_1_snapshot',
+    BOOK = 'book_ui_1'
+}
+
+export enum MESSAGE_PRODUCT_IDS {
+    PI_XBTUSD = 'PI_XBTUSD',
+    PI_ETHUSD = 'PI_ETHUSD',
+}
