2dd9548ce00c893f6ff0e3e8f746c9473ee0bf8c
bolthorn91 <bolthorn91@gmail.com>
2021-10-19 18:47:12 +0100

    Modify app component to show Orderbook title when orderbook is set

--- src/domain/types/types.ts
+++ src/domain/types/types.ts
@@ -1,3 +1,11 @@
+export interface IOrderbook {
+    asks: number[][];
+    bids: number[][];
+    feed: string;
+    numLevels?: number;
+    product_id: string;
+}
+
 export interface ISocketDataMessage {
     asks: number[][];
     bids: number[][];
