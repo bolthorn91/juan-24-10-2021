f7ca91d84165a3245228cb57effb7ee998b05499
bolthorn91 <bolthorn91@gmail.com>
2021-10-19 18:47:12 +0100

    Modify app component to show Orderbook title when orderbook is set

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getOrderbookSocket } from './domain/orders/getOrderbookSocket';
+import { IOrderbook } from 'domain/types/types';
+import './App.scss';
 
-function App() {
+export const App = () => {
+    const [orderbook, setOrderbook] = useState<IOrderbook | undefined>(undefined)
+
+    useEffect(() => {
+        getOrderbookSocket(setOrderbook)
+    }, [])
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            {orderbook && (
+                <h1>Order book</h1>
+            )}
+        </>
     );
 }
-
-export default App;
