b3df6fb1173622b306f372326d929380b91ad772
bolthorn91 <bolthorn91@gmail.com>
2021-10-19 18:39:31 +0100

    Add socket message enums

--- src/domain/orders/getOrderbookSocket.ts
+++ src/domain/orders/getOrderbookSocket.ts
@@ -1,11 +1,12 @@
 import { ISubscribeOrderbookDTO } from 'domain/types/dto';
+import { MESSAGE_EVENTS, MESSAGE_FEEDS, MESSAGE_PRODUCT_IDS } from 'domain/types/enums';
 import { ISocketDataMessage } from 'domain/types/types';
 
 export const getOrderbookSocket = (setOrderbook: Function): WebSocket => {
     const wsMessage: ISubscribeOrderbookDTO = {
-        event: 'subscribe',
-        feed: 'book_ui_1',
-        product_ids: ['PI_XBTUSD']
+        event: MESSAGE_EVENTS.SUBSCRIBE,
+        feed: MESSAGE_FEEDS.BOOK,
+        product_ids: [MESSAGE_PRODUCT_IDS.PI_XBTUSD]
     }
     const ws = new WebSocket('wss://www.cryptofacilities.com/ws/v1');
     ws.onopen = () => {
@@ -13,7 +14,7 @@ export const getOrderbookSocket = (setOrderbook: Function): WebSocket => {
     } 
     ws.onmessage = event => {
         const message: ISocketDataMessage = JSON.parse(event.data);
-        if (message.feed === 'book_ui_1_snapshot' && message.numLevels === 25) {
+        if (message.feed === MESSAGE_FEEDS.BOOK_SNAPSHOT && message.numLevels === 25) {
             setOrderbook(message);
         }
     }
